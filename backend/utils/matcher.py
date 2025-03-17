0c340a768f8094379c3c1619b188ea8cd44fc583
harshitsaxenaa <166727888+harshitsaxenaa@users.noreply.github.com>
2025-03-18 01:38:11 +0530

    Update matcher.py

--- backend/utils/matcher.py
+++ backend/utils/matcher.py
@@ -1,10 +1,18 @@
-
 from sentence_transformers import SentenceTransformer, util
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
-def match_cvs_to_jd(jd_text, cv_text):
+def match_cvs_to_jd(jd_text, cv_texts):
     jd_emb = model.encode(jd_text, convert_to_tensor=True)
-    cv_emb = model.encode(cv_text, convert_to_tensor=True)
-    score = util.pytorch_cos_sim(jd_emb, cv_emb).item()
-    return score
+    results = {}
+
+    for cv_filename, cv_text in cv_texts:
+        cv_emb = model.encode(cv_text, convert_to_tensor=True)
+        score = util.pytorch_cos_sim(jd_emb, cv_emb)
+
+        if score.dim() > 0:
+            score = score.mean().item() 
+
+        results[cv_filename] = {'match_score': score*100}
+
+    return results
